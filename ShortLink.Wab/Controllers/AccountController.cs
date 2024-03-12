ce71ba7ef1ae18f590349596f5df8753f1c5797d
mehdiparsa <49682743+mehdiparsa47@users.noreply.github.com>
2024-03-12 18:22:07 +0330

    SiteBaseController AccountController

---
+++ ShortLink.Wab/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShortLink.Application.DTOs.Account.LoginDto;
+using ShortLink.Application.DTOs.Account.RegisterDto;
+using ShortLink.Application.Services.Interfaces;
+
+namespace ShortLink.Wab.Controllers
+{
+    public class AccountController : SiteBaseController
+    {
+        #region ctor
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        #endregion
+
+        #region register
+        [HttpGet("register")]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpPost("register"),ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserDto registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+            return View(registerUser);
+        }
+
+        #endregion
+
+        #region login
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost("login"),ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginUser(LoginUserDto loginUser)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(loginUser);
+        }
+
+
+
+        #endregion
+
+
+        #region logout
+
+
+
+        #endregion
+    }
+}
---
+++ ShortLink.Wab/Controllers/SiteBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShortLink.Wab.Controllers
+{
+    public class SiteBaseController : Controller
+    {
+       
+    }
+}
