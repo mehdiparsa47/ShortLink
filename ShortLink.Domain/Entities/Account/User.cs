9539b833dbb1587733a41213ad3dfb852f27a5f7
mehdiparsa <49682743+mehdiparsa47@users.noreply.github.com>
2024-03-12 15:41:49 +0330

    User Entity

---
+++ ShortLink.Domain/Entities/Account/User.cs
@@ -0,0 +1,37 @@
+﻿using ShortLink.Domain.Entities.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShortLink.Domain.Entities.Account;
+
+public class User : BaseEntity
+{
+    [Display(Name = "تلفن همراه")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string Mobile { get; set; }
+
+    [Display(Name = "نام")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string FirstName { get; set; }
+
+    [Display(Name = "نام خانوادگی")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string LastName { get; set; }
+
+    [Display(Name = "کلمه ی عبور")]
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+    [MaxLength(20, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+    public string MobileActiveCode { get; set; }
+
+    [Display(Name = "موبایل فعال / غیر فعال")]
+    public bool IsMobileActive { get; set; }
+
+    [Display(Name = "بلاک شده / نشده")]
+    public bool IsBlocked { get; set; }
+}
